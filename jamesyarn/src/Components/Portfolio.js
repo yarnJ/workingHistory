a2129dadc588750b7d29b2bf038e33c0001cf861
yarnJ <yarnJ33@gmail.com>
2021-12-18 06:51:12 -0800

    james personal porfolio

---
+++ jamesyarn/src/Components/Portfolio.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Zmage from "react-zmage";
+import Fade from "react-reveal";
+
+let id = 0;
+class Portfolio extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const projects = this.props.data.projects.map(function (projects) {
+      let projectImage = "images/portfolio/" + projects.image;
+
+      return (
+        <div key={id++} className="columns portfolio-item">
+          <div className="item-wrap">
+            <Zmage alt={projects.title} src={projectImage} />
+            <div style={{ textAlign: "center" }}>
+              <a href={ projects.url }>
+              {projects.title}
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <section id="portfolio">
+        <Fade left duration={1300} distance="20px">
+          <div className="row">
+            <div className="twelve columns collapsed">
+              <h1>Check Out Some of My Works.</h1>
+
+              <div
+                id="portfolio-wrapper"
+                className="bgrid-quarters s-bgrid-thirds cf"
+              >
+                {projects}
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
